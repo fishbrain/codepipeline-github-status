6e0ae0ad1d88e730075f6d3e2826250a5ed0cb46
Erik Dalén <erik.gustav.dalen@gmail.com>
2018-11-12 14:24:06 +0100

    Switch to serverless-webpack

---
+++ webpack.config.js
@@ -0,0 +1,29 @@
+const path = require('path');
+const slsw = require('serverless-webpack');
+
+module.exports = {
+  mode: slsw.lib.webpack.isLocal ? 'development' : 'production',
+  entry: slsw.lib.entries,
+  devtool: 'inline-source-map',
+  resolve: {
+    extensions: ['.js', '.jsx', '.json', '.ts', '.tsx'],
+  },
+  output: {
+    libraryTarget: 'commonjs',
+    path: path.join(__dirname, '.webpack'),
+    filename: '[name].js',
+  },
+  target: 'node',
+  module: {
+    rules: [
+      // all files with a `.ts` or `.tsx` extension will be handled by `ts-loader`
+      { test: /\.tsx?$/, loader: 'ts-loader' },
+      {
+        type: 'javascript/auto',
+        test: /\.mjs$/,
+        use: [],
+      },
+    ],
+  },
+  externals: ['aws-sdk'],
+};
